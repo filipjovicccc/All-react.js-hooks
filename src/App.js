cffcf067b2406c0c79adca3513e97f0e725402f9
filipjovicccc <filipjovicccc@gmail.com>
2022-05-07 16:51:22 +0200

    add reducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
 function App() {
   // prvi zadatak postavuti counter pomocu use stejta
@@ -7,21 +7,62 @@ function App() {
   //treci zadatak iskoristiti useReducer udicu, za jedno dugme koje
   //je brojac i prikazuje text
 
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
 
-  const [stat, setStat] = useState("example"); //useState hook
+  // const [stat, setStat] = useState("example"); //useState hook
 
-  function setInputHandler(e) {
-    const newStat = e.target.value;
-    setStat(newStat);
-  }
+  // const reducer = (state, action) => {
+  //   switch (action.type) {
+  //     case "incriment":
+  //       return {
+  //         showText: state.showText,
+  //         reducerCounter: state.reducerCounter + 1,
+  //       };
+  //     case "decriment":
+  //       return {
+  //         showText: state.showText,
+  //         reducerCounter: state.reducerCounter - 1,
+  //       };
+  //     case "showText":
+  //       return {
+  //         showText: !state.showText,
+  //         reducerCounter: state.reducerCounter,
+  //       };
+  //     default:
+  //       return state;
+  //   }
+  // };
+
+  // function setInputHandler(e) {
+  //   const newStat = e.target.value;
+  //   setStat(newStat);
+  // }
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return {
+          count: state.count + 1,
+          showText: state.showText,
+        };
+      case "showText":
+        return { cont: state.count, showText: !state.showText };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    showText: true,
+  });
 
   return (
     <div className="App">
       <header className="bg-dark text-white p-4 mb-2"></header>
       <form className="form">
         <h1>Hello from react</h1>
-        <span className="span">{counter}</span>
+        {/* <span className="span">{counter}</span>
         <button
           type="button"
           onClick={() => setCounter(counter + 1)}
@@ -35,23 +76,32 @@ function App() {
           onClick={() => setCounter(counter - 1)}
           class="btn btn-danger"
         >
-          Decriment
-        </button>
-        <input type="text" onChange={setInputHandler} />
+          Decriment */}
+        {/* </button> */}
+        {/* <input type="text" onChange={setInputHandler} />
         <span>
           <p>your example is here: {stat}</p>
-        </span>
+        </span> */}
 
-        <button type="button" class="btn btn-secondary">
-          Use reduser dugme
+        {state.showText && <p>Show this text</p>}
+        <button
+          type="button"
+          onClick={() => {
+            dispatch({ type: "INCREMENT" });
+            dispatch({ type: "showText" });
+          }}
+          // onClick={() => {
+          //   dispatch({ type: "incriment" });
+          //   dispatch({ type: "showText" });
+          // }}
+          class="btn btn-secondary"
+        >
+          Use reducer dugme
         </button>
-
-        <span className="span">{state.reducerCounter}</span>
-        <span>{show}</span>
+        {state.count}
       </form>
     </div>
   );
 }
 
 export default App;
-state.reducerCounter;
