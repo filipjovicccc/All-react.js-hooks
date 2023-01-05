c84cd626aa7ab38dfd9ef38263bbe32c30057d9f
filipjovicccc <filipjovicccc@gmail.com>
2023-01-05 23:01:49 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,113 +1,27 @@
-import "./App.css";
-import { useState, useReducer } from "react";
+import { useState } from "react";
+import Reducer from "./components/UseReducer"
+import Effects from "./components/UseEffect";
 
-import UseEffect from "./components/UseEffect";
 
-function App() {
-  // prvi zadatak postavuti counter pomocu use stejta
-  // drugi zadatak input
-  //treci zadatak iskoristiti useReducer udicu, za jedno dugme koje
-  //je brojac i prikazuje text
+const App = () =>{
+  //1.Zadatak / Counter increasment useState
+  const [counter, setCounter] = useState(0)
 
-  // const [counter, setCounter] = useState(0);
+  const counterPlus = () => {
+  setCounter(counter +1)
+  }
+ 
+ return <div className="form">
+    <h1>Hello world</h1>
+ 
+    <div>
+    {counter}<button onClick={counterPlus} >Increment</button>
 
-  // const [stat, setStat] = useState("example"); //useState hook
-
-  // const reducer = (state, action) => {
-  //   switch (action.type) {
-  //     case "incriment":
-  //       return {
-  //         showText: state.showText,
-  //         reducerCounter: state.reducerCounter + 1,
-  //       };
-  //     case "decriment":
-  //       return {
-  //         showText: state.showText,
-  //         reducerCounter: state.reducerCounter - 1,
-  //       };
-  //     case "showText":
-  //       return {
-  //         showText: !state.showText,
-  //         reducerCounter: state.reducerCounter,
-  //       };
-  //     default:
-  //       return state;
-  //   }
-  // };
-
-  // function setInputHandler(e) {
-  //   const newStat = e.target.value;
-  //   setStat(newStat);
-  // }
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "INCREMENT":
-        return {
-          count: state.count + 1,
-          showText: state.showText,
-        };
-      case "showText":
-        return { count: state.count, showText: !state.showText };
-      default:
-        return state;
-    }
-  };
-
-  const [state, dispatch] = useReducer(reducer, {
-    count: 0,
-    showText: true,
-  });
-
-  return (
-    <div className="App">
-      <header className="bg-dark text-white p-4 mb-2"></header>
-      <form className="form">
-        <h1>Hello from react</h1>
-        {/* <span className="span">{counter}</span>
-        <button
-          type="button"
-          onClick={() => setCounter(counter + 1)}
-          class="btn btn-primary"
-        >
-          Incriment
-        </button>
-
-        <button
-          type="button"
-          onClick={() => setCounter(counter - 1)}
-          class="btn btn-danger"
-        >
-          Decriment */}
-        {/* </button> */}
-        {/* <input type="text" onChange={setInputHandler} />
-        <span>
-          <p>your example is here: {stat}</p>
-        </span> */}
-
-        <UseEffect />
-
-        {state.showText && <p>Show this text</p>}
-        <button
-          type="button"
-          onClick={() => {
-            dispatch({ type: "INCREMENT" });
-            dispatch({ type: "showText" });
-          }}
-          // onClick={() => {
-          //   dispatch({ type: "incriment" });
-          //   dispatch({ type: "showText" });
-          // }}
-          className="btn btn-secondary"
-        >
-          Use reducer dugme
-        </button>
-        <div>
-          <span className="span">{state.count}</span>
-        </div>
-      </form>
     </div>
-  );
+    <Reducer />
+    <Effects />
+ </div>
+
 }
 
-export default App;
+export default App
--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,32 +1,26 @@
-import React from "react";
-import { useEffect, useState } from "react";
-// zadatak fetching data sa publick apija i prikazati na ekranu
-import axios from "axios";
+import React, {useEffect, useState} from "react";
+import axios from "axios"
 
-function UseEffect() {
+function Effects() {
   const [data, setData] = useState("");
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/comments")
-      .then((response) => {
-        setData(response.data[0].email);
-        console.log("API WAS CALLED");
-      });
-  }, [count]);
+  const [count, setCount] = useState(0)
 
-  return (
-    <div>
-      Hello form {data} and useEffect succses also {count}
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Add count
-      </button>
-    </div>
-  );
+  useEffect(()=> {
+    axios.get("https://jsonplaceholder.typicode.com/comments").then((response)=> {
+         setData(response.data[0].email)
+         console.log("API WAS CALLED")
+    })
+  },[count])
+
+  return <div>Hello World {data}
+  <h1>{count}</h1>
+  <button onClick={()=> {
+    setCount(count +1);
+  }}>Click</button>
+
+  </div>
+
+
 }
 
-export default UseEffect;
+export default Effects
